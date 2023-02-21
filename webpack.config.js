063bbaf7ee16221070726cc4ac66ba0f1c852bb3
Artem <artemmakarov76@yandex.ru>
2023-02-21 19:38:56 +0300

    feat: shit does not work damn

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.jsx",
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 3000,
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].jsx",
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      // filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-react','@babel/preset-env']
+          }
+        }
+      },
+    ]
+  }
+}
